c1ea6b53bdb790a151f478d07a681e7b9b74d93c
Echo343 <Echo343@users.noreply.github.com>
2016-11-23 15:04:02 -0800

    fixes #1
    Catching NPE on debug messages

--- src/main/java/com/blargsworkshop/airmattress/events/EventHandlers.java
+++ src/main/java/com/blargsworkshop/airmattress/events/EventHandlers.java
@@ -14,34 +14,25 @@ import net.minecraft.util.ChunkCoordinates;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 
 public class EventHandlers {
-	static final HashMap<String, Boolean> flag = new HashMap<String, Boolean>();
+	static final HashMap<String, Boolean> isPlayerWakingUpMap = new HashMap<String, Boolean>();
 	static final HashMap<String, ChunkCoordinates> spawnPoints = new HashMap<String, ChunkCoordinates>();
-//	static int count = 0;
 	
+	/**
+	 * When a player wakes from bed, there spawn point is not yet set.
+	 * Grab this old coordinate and save to restore after the new spawn is set.
+	 * @param event
+	 */
 	@SubscribeEvent(priority=EventPriority.NORMAL, receiveCanceled=true)
 	public void onEvent(PlayerWakeUpEvent event) {
 		if (!event.entityPlayer.worldObj.isRemote) {
-			
-			//reset count if needed
-//			boolean isFound = false;
-//			for (Boolean b : flag.values()) {
-//				if (b.equals(Boolean.TRUE)) {
-//					isFound = true;
-//					break;
-//				}
-//			}
-//			if (!isFound) {
-//				count = 0;
-//			}
-			
 			EntityPlayer player = event.entityPlayer;
 			AirMattressMod.debug("Waking" + event.entityPlayer.getDisplayName(), 3, player);
-			AirMattressMod.debug("Old Spawn for " + player.getDisplayName() + ": " + player.getBedLocation(player.dimension), 2, player);
+			AirMattressMod.debug("Old Spawn for " + player.getDisplayName() + ": " + (player.getBedLocation(player.dimension) != null ? player.getBedLocation(player.dimension) : "null"), 2, player);
 			
+			//A player's spawn point is not yet set right when he/she wakes up.
 			ChunkCoordinates oldSpawnPoint = player.getBedLocation(player.dimension);
 			spawnPoints.put(player.getDisplayName(), oldSpawnPoint);
-			flag.put(player.getDisplayName(), Boolean.TRUE);
-//			count++;
+			isPlayerWakingUpMap.put(player.getDisplayName(), Boolean.TRUE);
 		}
 	}
 	
@@ -60,34 +51,35 @@ public class EventHandlers {
 	@SubscribeEvent(priority=EventPriority.NORMAL, receiveCanceled=true)
 	public void onEvent(PlayerTickEvent event) {
 		EntityPlayer player = event.player;
-		if (event.player.worldObj.isRemote == false && event.phase == Phase.START && Boolean.TRUE.equals(flag.get(player.getDisplayName()))) {
-//			if (flag.get(player.getDisplayName()).equals(Boolean.TRUE)) {
-				AirMattressMod.debug("Post Wake for " + event.player.getDisplayName(), 3, player);
-				flag.put(player.getDisplayName(), Boolean.FALSE);
-//				count--;
-				
-				ChunkCoordinates bedPosition = player.getBedLocation(player.dimension);
-				if (bedPosition != null) {
-					Block bedType = player.worldObj.getBlock(bedPosition.posX, bedPosition.posY, bedPosition.posZ);
-					if (bedType != null) {
-						if (bedType == AirMattressMod.airMattressBlock) {
-							AirMattressMod.debug(player.getDisplayName() + " slept on an AirMattress", 1, player);
-							
-							ChunkCoordinates newSpawnPoint = player.getBedLocation(player.dimension);
-							ChunkCoordinates oldSpawnPoint = spawnPoints.get(player.getDisplayName());
-							if (!newSpawnPoint.equals(oldSpawnPoint)) {
-								player.setSpawnChunk(oldSpawnPoint, false);
-								AirMattressMod.debug("Spawn point adjustment for " + player.getDisplayName() + ": " + ((oldSpawnPoint.equals(player.getBedLocation(player.dimension))) ? "Successful" : "Failed"), 1, player);
-							}
-							
+		if (event.player.worldObj.isRemote == false && event.phase == Phase.START && Boolean.TRUE.equals(isPlayerWakingUpMap.get(player.getDisplayName()))) {
+			AirMattressMod.debug("Post Wake for " + event.player.getDisplayName(), 3, player);
+			//clear the waking flag so this doesn't run for every tick
+			isPlayerWakingUpMap.put(player.getDisplayName(), Boolean.FALSE);
+			
+			ChunkCoordinates bedPosition = player.getBedLocation(player.dimension);
+			if (bedPosition != null) {
+				Block bedType = player.worldObj.getBlock(bedPosition.posX, bedPosition.posY, bedPosition.posZ);
+				if (bedType != null && bedType == AirMattressMod.airMattressBlock) {
+					AirMattressMod.debug(player.getDisplayName() + " slept on an AirMattress", 1, player);
+					
+					ChunkCoordinates newSpawnPoint = player.getBedLocation(player.dimension);
+					ChunkCoordinates oldSpawnPoint = spawnPoints.get(player.getDisplayName());
+					if (!newSpawnPoint.equals(oldSpawnPoint)) {
+						player.setSpawnChunk(oldSpawnPoint, false);
+						ChunkCoordinates verifyBedLocation = player.getBedLocation(player.dimension);
+						if ((oldSpawnPoint != null && oldSpawnPoint.equals(verifyBedLocation)) || (oldSpawnPoint == null && verifyBedLocation == null)) {
+							AirMattressMod.debug("Spawn point adjustment for " + player.getDisplayName() + ": Successful", 1, player);
 						}
 						else {
-							AirMattressMod.debug(player.getDisplayName() + " slept on an " + bedType.toString(), 1, player);
+							AirMattressMod.debug("Spawn point adjustment for " + player.getDisplayName() + ": Failed", 1, player);
 						}
-						AirMattressMod.debug("New Spawn for " + player.getDisplayName() + ": " + player.getBedLocation(player.dimension), 2, player);
 					}
 				}
-//			}
+				else {
+					AirMattressMod.debug(player.getDisplayName() + " slept on an " + (bedType != null ? bedType.toString() : "nullBed"), 1, player);
+				}
+				AirMattressMod.debug("New Spawn for " + player.getDisplayName() + ": " + (player.getBedLocation(player.dimension) != null ? player.getBedLocation(player.dimension) : "null"), 2, player);
+			}
 		}
 	}
 }
