05232c9bfd96e0308f51bfc523a42dd6bb00e10a
Echo343 <Echo343@users.noreply.github.com>
2016-11-23 15:06:10 -0800

    Version update

--- src/main/java/com/blargsworkshop/airmattress/AirMattressMod.java
+++ src/main/java/com/blargsworkshop/airmattress/AirMattressMod.java
@@ -30,7 +30,7 @@ public class AirMattressMod
 {
     public static final String MODID = "blarg_airmattressmod";
     public static final String NAME = "AirMattress";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
     
     private static final int DEBUG_LVL = 0;
     private static final boolean DEBUG_CHAT = true;
